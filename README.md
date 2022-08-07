3f9c128fea9a9711e7422e6892b52ef4aeb62723
AlexRose97 <aexlopez@gmail.com>
2022-08-07 12:21:35 -0600

    Creacion de distintos componentes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Veterinaria_Vite_React
+
 Proyecto para practicar React utilizando Vite
+
+- npm install
+- npm run dev
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,9 @@
+function ListadoPacientes() {
+  return (
+    <>
+      <h2>Pacientes</h2>
+    </>
+  );
+}
+
+export default ListadoPacientes;
